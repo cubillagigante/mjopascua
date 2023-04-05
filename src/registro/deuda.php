d2465c0c2416bdd743aaf1ea94c4ea87990caaf1
Diego <diegocubilla2001@gmail.com>
2023-04-05 15:51:31 -0400

    añadir buscador a deuda y cambiar delete por update en eliminar_deuda

--- src/registro/deuda.php
+++ src/registro/deuda.php
@@ -5,6 +5,15 @@
   $id_usuario = $_SESSION['id_login'];
   $tipo_user = $_SESSION['status'];
 
+  $and = '';
+  if(!empty($_POST))
+  {
+    $valor = $_POST['nombre_apellido'];
+    if(!empty($valor)){
+      $and = "where nombre_deuda LIKE '%$valor%'";
+    }
+  }
+
   if(!isset($usuario_sesion))
   {
     header("location: ../login/index.php");
@@ -13,7 +22,7 @@
   } 
 
 
-  $sqlColor = "SELECT * FROM deuda ORDER BY monto DESC";
+  $sqlColor = "SELECT * FROM deuda $and ORDER BY actividad DESC";
   $RColor = $mysqli->query($sqlColor);
  
 ?>
@@ -35,28 +44,46 @@
                 <img src="../../public/images/default/logo.png" class="rounded-full w-20 mx-auto p-2 mb-5" />
                 <form action="../../php/guardar_deuda.php" method="post">
                     <div class="flex gap-5 items-center justify-center mb-5">
-                    
+
                         <div class="grid gap-5">
-                            <h1 class="font-bold text-xl">Nombre:</h1>
-                            <input type="text" name="nombre" class="block text-black text-center rounded-full p-5" value=""
-                                placeholder="ej: Camila Coronel" required/>
-                            <h1 class="font-bold text-xl">Monto:</h1>
+
+                            <input type="text" name="nombre" class="block text-black text-center rounded-full p-5"
+                                value="" placeholder="ej: Camila Coronel" required />
+
                             <input type="tel" name="deuda" class=" text-black text-center rounded-full p-5" value=""
-                                placeholder="ej: 2000" required/>
+                                placeholder="ej: 2000" required />
+
+
                         </div>
-                        <label class="btn w-20 text-white text-center rounded-full p-5" for="btn-ok">
-                        
-                            <input id="btn-ok" type="submit" name="btn-ok" class="" value=""
-                             /><i class="ti ti-circle-plus"></i>
-                        
+
+                        <label class="block btn w-20 text-white text-center rounded-full p-5" for="btn-ok">
+
+                            <input id="btn-ok" type="submit" name="btn-ok" class="" value="" /><i
+                                class="ti ti-circle-plus"></i>
+
                         </label>
+
+
+                    </div>
+                </form>
+                <form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
+                    <div class="flex gap-5 justify-center mb-5">
+                        <input type="text" name="nombre_apellido" class=" text-white bg-gray-800 text-center rounded-full p-5" value=""
+                            placeholder="Buscar por nombre" />
+                        <label class="block btn w-20 text-white text-center rounded-full p-5" for="btn_search">
+
+                            <input id="btn_search" type="submit" class="" value="" /><i
+                                class="ti ti-search"></i>
+
+                        </label>
+
                     </div>
                 </form>
                 <table class=" w-full text-sm rounded-lg text-left text-[#EFE4AB]">
 
                     <thead class=" rounded-lg text-[#EFE4AB] text-xl border-b  uppercase text-center  ">
                         <tr>
-                           
+
                             <th scope="col" class=" py-3">
                                 Nombre
                             </th>
@@ -64,26 +91,31 @@
                                 Deuda
                             </th>
                             <th scope="col-2" class="px-2 py-3">
-                                
+
                             </th>
-                          
+
                         </tr>
                     </thead>
                     <tbody id="cuerpo">
                         <?php $con =0;  while($row = $RColor->fetch_array(MYSQLI_ASSOC)) { $con++; ?>
                         <form action="../../php/guardar_puntos.php" method="post">
                             <tr class="text-white font-bold text-center hover:bg-[#efd788c0] ">
-                                
+
                                 <td class=" py-4">
                                     <?php  echo $row['nombre_deuda']; ?>
-                                    
+
                                 </td>
                                 <td class=" py-4">
                                     <?php  echo $row['monto']; ?> Gs.
-                                    
+
                                 </td>
                                 <td class=" py-4">
-                                    <a href="../../php/eliminar_deuda.php?id=<?php  echo $row['id_deuda']; ?>"><i class="bg-red-500 p-2 rounded-full ti ti-eraser"></i></a>
+                                    <?php if($row['actividad'] == 0) {
+                                       echo '<h1 class="text-green-400">Pagado<h1>';
+                                    } else { ?>
+                                    <a href="../../php/eliminar_deuda.php?id=<?php  echo $row['id_deuda']; ?>"><i
+                                            class="bg-red-500 p-2 rounded-full ti ti-eraser"></i></a>
+                                    <?php } ?>
                                 </td>
                             </tr>
                         </form>
@@ -92,7 +124,7 @@
                 </table>
 
             </div>
-            
+
 
         </div>
     </div>
@@ -100,8 +132,4 @@
 
     </div>
 </body>
-
 </html>
-<script>
-
-</script>
